c98c9788ae416eb571762efdb1627218d850e06d
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-09-13 14:38:21 +0200

    Remove debug

--- rancher.groovy
+++ rancher.groovy
@@ -4,11 +4,6 @@ def withEnvironment(url, cred, body) {
         credentialsId: cred,
         usernameVariable: 'RANCHER_ACCESS_KEY',
         passwordVariable: 'RANCHER_SECRET_KEY']]) {
-
-      sh 'echo "debug: RANCHER_URL=$RANCHER_URL"'
-      sh 'echo "debug: RANCHER_ACCESS_KEY=$RANCHER_ACCESS_KEY"'
-      sh 'echo "debug: RANCHER_SECRET_KEY=$RANCHER_SECRET_KEY"'
-
       body.call()
     }
   }
